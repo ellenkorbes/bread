250c84b57a4259dab7673d9da4a3134d4a0d10eb
Ellen Körbes <ellenkorbes@gmail.com>
2019-01-28 23:36:22 +0100

    croissant

--- bread.go
+++ bread.go
@@ -1,10 +1,10 @@
 package bread
 
-// 1.1.0-sliced
+// 1.1.0-croissant
 // 1.2.0-bruschetta
 // 2.0.0-croissant
 
 // Slice is a unit of bread.
 func Slice() string {
-	return "sliced"
+	return "croissant"
 }
